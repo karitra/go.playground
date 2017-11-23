57c3eab9c0ee4b09ace6b32568d85ca5a60395f0
karitra <karev.alexandre@gmail.com>
2017-11-23 21:28:29 +0300

    refact: update cgroups benchs

--- bench/cgroups_test.go
+++ bench/cgroups_test.go
@@ -1,15 +1,51 @@
 package bench
 
 import (
-    // "github.com/docker/docker/client"
-    // "github.com/yandex/porto/blob/master/src/api/go"
-    // "fmt"
+    _ "github.com/docker/docker/client"
+    "porto" // local link to "github.com/yandex/porto/src/api/go/porto"
     "testing"
+    "fmt"
     "bench.met/bench/common"
 )
 
-func BenchmarkDecayMetrics1k(b *testing.B) {
+var (
+    containerPorto = "dork.porto"
+    containerDocker = "dork.docker"
+)
+
+
+func dumpPortoResult(r *map[string]map[string]porto.TPortoGetResponse) {
+    for app, v := range *r {
+        for metric, value := range v {
+            if value.Error != 0 {
+                fmt.Printf("response error [%d] %s\n", value.Error, value.ErrorMsg)
+            } else {
+                fmt.Printf("%s.%s %s\n", app, metric, value.Value)
+            }
+        }
+    }
+}
+
+func BenchmarkPorto(b *testing.B) {
+
+    containers := []string{containerPorto}
+
+    props := []string{
+        "cpu_usage",
+        "memory_usage",
+    }
+
+    api, err := porto.Connect()
+    if err != nil {
+        panic(err)
+    }
+
     common.RunParallel(b, func() {
-        _ := 1 + 1
+        resp, err := api.Get(containers, props)
+        if err != nil {
+            fmt.Printf("failed to get metrics %v\n", err)
+            b.FailNow()
+        }
+        dumpPortoResult(&resp)
     })
 }
